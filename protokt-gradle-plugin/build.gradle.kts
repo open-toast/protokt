e2c49fda2151b2eae5b1e49292b311319e10e75f
Oleg Golberg <ogolberg@toasttab.com>
2020-02-22 08:55:07 -0500

    Fix publishing to Gradle Plugin Portal

--- protokt-gradle-plugin/build.gradle.kts
+++ protokt-gradle-plugin/build.gradle.kts
@@ -50,6 +50,10 @@ pluginBundle {
     tags = listOf("protobuf", "kotlin")
 }
 
+
+ext[com.gradle.publish.PublishTask.GRADLE_PUBLISH_KEY] = System.getenv("GRADLE_PORTAL_PUBLISH_KEY")
+ext[com.gradle.publish.PublishTask.GRADLE_PUBLISH_SECRET] = System.getenv("GRADLE_PORTAL_PUBLISH_SECRET")
+
 tasks.named("publishPlugins") {
     enabled = isRelease()
 }
