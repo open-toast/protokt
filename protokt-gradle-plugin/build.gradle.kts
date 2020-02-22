43bebfbba77d0ff45b26ff74f1339a0dda602bec
Oleg Golberg <ogolberg@toasttab.com>
2020-02-22 09:10:13 -0500

    lint

--- protokt-gradle-plugin/build.gradle.kts
+++ protokt-gradle-plugin/build.gradle.kts
@@ -50,7 +50,6 @@ pluginBundle {
     tags = listOf("protobuf", "kotlin")
 }
 
-
 ext[com.gradle.publish.PublishTask.GRADLE_PUBLISH_KEY] = System.getenv("GRADLE_PORTAL_PUBLISH_KEY")
 ext[com.gradle.publish.PublishTask.GRADLE_PUBLISH_SECRET] = System.getenv("GRADLE_PORTAL_PUBLISH_SECRET")
 
