08c4a8483e4a1537b4e6dd4be6163b37d3de243c
Andrew Parmet <andrew@parmet.com>
2021-06-27 21:16:27 -0400

    add copyright headers for Toast/gRPC in appropriate places

--- examples/grpc-java/src/main/kotlin/io/grpc/examples/routeguide/RouteGuideClient.kt
+++ examples/grpc-java/src/main/kotlin/io/grpc/examples/routeguide/RouteGuideClient.kt
@@ -1,9 +1,11 @@
 /*
- * Copyright (c) 2021 Toast Inc.
+ * Copyright 2020 gRPC authors.
+ * Copyright 2021 Toast Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
