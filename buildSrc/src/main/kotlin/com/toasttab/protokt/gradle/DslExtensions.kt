7452da7a41739e9ab0c0525c8291b6674c40e4ae
Andrew Parmet <andrew@parmet.com>
2021-08-02 14:18:31 -0400

    add missing configuration extensions

--- buildSrc/src/main/kotlin/com/toasttab/protokt/gradle/DslExtensions.kt
+++ buildSrc/src/main/kotlin/com/toasttab/protokt/gradle/DslExtensions.kt
@@ -17,7 +17,10 @@ package com.toasttab.protokt.gradle
 
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.ExternalModuleDependency
+import org.gradle.api.artifacts.ModuleDependency
 import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.gradle.kotlin.dsl.add
 import org.gradle.kotlin.dsl.configure
 
 fun Project.protokt(cfg: ProtoktExtension.() -> Unit) = project.configure(cfg)
@@ -25,5 +28,37 @@ fun Project.protokt(cfg: ProtoktExtension.() -> Unit) = project.configure(cfg)
 fun DependencyHandler.protoktExtensions(dependencyNotation: Any): Dependency? =
     add(EXTENSIONS, dependencyNotation)
 
+inline fun <T : ModuleDependency> DependencyHandler.protoktExtensions(
+    dependencyNotation: T,
+    dependencyConfiguration: T.() -> Unit
+): Dependency =
+    add(EXTENSIONS, dependencyNotation, dependencyConfiguration)
+
+inline fun DependencyHandler.protoktExtensions(
+    dependencyNotation: String,
+    dependencyConfiguration: ExternalModuleDependency.() -> Unit
+): ExternalModuleDependency =
+    add(
+        EXTENSIONS,
+        create(dependencyNotation) as ExternalModuleDependency,
+        dependencyConfiguration
+    )
+
 fun DependencyHandler.testProtoktExtensions(dependencyNotation: Any): Dependency? =
     add(TEST_EXTENSIONS, dependencyNotation)
+
+inline fun <T : ModuleDependency> DependencyHandler.testProtoktExtensions(
+    dependencyNotation: T,
+    dependencyConfiguration: T.() -> Unit
+): Dependency =
+    add(TEST_EXTENSIONS, dependencyNotation, dependencyConfiguration)
+
+inline fun DependencyHandler.testProtoktExtensions(
+    dependencyNotation: String,
+    dependencyConfiguration: ExternalModuleDependency.() -> Unit
+): ExternalModuleDependency =
+    add(
+        TEST_EXTENSIONS, 
+        create(dependencyNotation) as ExternalModuleDependency,
+        dependencyConfiguration
+    )
