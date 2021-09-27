fbf8bb56269bbe3e53a24ab461e59041ca2a7506
Andrew Parmet <andrew@parmet.com>
2021-09-27 09:56:33 -0400

    more wip

--- protokt-core/src/main/kotlin/com/toasttab/protokt/Descriptors.kt
+++ protokt-core/src/main/kotlin/com/toasttab/protokt/Descriptors.kt
@@ -17,10 +17,6 @@ package com.toasttab.protokt
 
 import com.toasttab.protokt.rt.finishList
 
-fun foo() {
-    DescriptorProto.ExtensionRange.descriptor
-}
-
 class FileDescriptor(
     val proto: FileDescriptorProto,
     val dependencies: List<FileDescriptor>
