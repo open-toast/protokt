0f9e16e264f2b08058182a5546a190bac242d0bc
Andrew Parmet <andrew@parmet.com>
2022-04-13 15:20:47 -0400

    upgrade some libs

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -23,11 +23,11 @@ repositories {
 }
 
 dependencies {
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:5.15.1")
-    implementation("com.google.protobuf:protobuf-gradle-plugin:0.8.17")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.3.0")
+    implementation("com.google.protobuf:protobuf-gradle-plugin:0.8.18")
     implementation("io.codearte.gradle.nexus:gradle-nexus-staging-plugin:0.30.0")
-    implementation("com.google.guava:guava:30.1.1-jre")
+    implementation("com.google.guava:guava:31.1-jre")
     implementation("ru.vyarus:gradle-animalsniffer-plugin:1.5.4")
-    implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.7.0")
+    implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.8.0")
     implementation(kotlin("gradle-plugin-api"))
 }
