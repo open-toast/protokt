880623d42141db43bf97d59685e71a02b26feb39
Andrew Parmet <andrew@parmet.com>
2022-05-31 17:10:37 -0400

    revert unneeded changes

--- protokt-codegen/build.gradle.kts
+++ protokt-codegen/build.gradle.kts
@@ -14,6 +14,7 @@
  */
 
 import com.google.protobuf.gradle.GenerateProtoTask
+import com.google.protobuf.gradle.proto
 import com.google.protobuf.gradle.protobuf
 import com.google.protobuf.gradle.protoc
 import com.toasttab.protokt.gradle.CODEGEN_NAME
@@ -50,8 +51,6 @@ dependencies {
 
     testImplementation(libraries.junit)
     testImplementation(libraries.truth)
-
-    protobuf(files("../extensions/protokt-extensions-lite/src/main/proto"))
 }
 
 configure<PublishingExtension> {
@@ -81,6 +80,14 @@ tasks.withType<Test> {
     doFirst {
         tasks.named<GenerateProtoTask>("generateProto") {
             environment("PROTOC_PATH", locatorToAlternativePathsMapping.getting("protoc").get().singleFile)
+        }
+    }
+}
+
+sourceSets {
+    main {
+        proto {
+            srcDir("../extensions/protokt-extensions-lite/src/main/proto")
         }
     }
 }
