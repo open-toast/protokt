29924a74bb6fcb1a8f7e3962bf4521e89c50319c
Andrew Parmet <andrew@parmet.com>
2023-05-25 11:27:38 -0400

    copypasta

--- protokt-codegen/src/main/kotlin/com/toasttab/protokt/v1/codegen/util/GeneratedCodeCleanup.kt
+++ protokt-codegen/src/main/kotlin/com/toasttab/protokt/v1/codegen/util/GeneratedCodeCleanup.kt
@@ -36,7 +36,7 @@ private fun stripApiMode(code: String) =
         .replace("public abstract ", "abstract ")
         .replace("public open ", "open ")
         .replace("public final ", "final ")
-        .replace("public const ", "final ")
+        .replace("public const ", "const ")
         .replace("public val ", "val ")
         .replace("public var ", "var ")
         .replace("public fun ", "fun ")
