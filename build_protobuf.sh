048bd72dabae8d2945b9ac3936fdf56a9cfc6d56
Andrew Parmet <andrew@parmet.com>
2023-09-05 10:55:58 -0400

    fix script

--- build_protobuf.sh
+++ build_protobuf.sh
@@ -1,1 +1,6 @@
-test -f ci/protobuf/conformance_test_runner || { cd ci/protobuf && git submodule update --init --recursive && cmake . -DCMAKE_CXX_STANDARD=14 -Dprotobuf_BUILD_CONFORMANCE=ON && cmake --build . }
+if [ ! -f "ci/protobuf/conformance_test_runner" ]; then
+    cd ci/protobuf
+    git submodule update --init --recursive
+    cmake . -DCMAKE_CXX_STANDARD=14 -Dprotobuf_BUILD_CONFORMANCE=ON
+    cmake --build .
+fi
