e08f6ab3fd6e5fb10fb81cfc5d237a42dfdc5bd0
Andrew Parmet <andrew@parmet.com>
2023-10-10 14:35:08 -0400

    extract bytes

---
+++ protokt-runtime/src/commonMain/kotlin/protokt/v1/AbstractBytes.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2023 Toast, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package protokt.v1
+
+import kotlin.jvm.JvmStatic
+
+abstract class AbstractBytes internal constructor(
+    internal val value: ByteArray
+) {
+    val bytes
+        get() = clone(value)
+
+    fun isNotEmpty() =
+        value.isNotEmpty()
+
+    fun isEmpty() =
+        value.isEmpty()
+
+    fun toBytesSlice() =
+        BytesSlice(value)
+
+    final override fun equals(other: Any?) =
+        other is Bytes && value.contentEquals(other.value)
+
+    final override fun hashCode() =
+        value.contentHashCode()
+
+    final override fun toString() =
+        value.contentToString()
+
+    internal companion object {
+        private val EMPTY = Bytes(ByteArray(0))
+
+        @JvmStatic
+        fun empty() =
+            EMPTY
+
+        @JvmStatic
+        fun from(bytes: ByteArray) =
+            Bytes(clone(bytes))
+
+        @JvmStatic
+        fun from(message: KtMessage) =
+            Bytes(message.serialize())
+    }
+}
---
+++ protokt-runtime/src/jvmMain/kotlin/protokt/v1/Bytes.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2023 Toast, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package protokt.v1
+
+import java.io.InputStream
+import java.nio.ByteBuffer
+import kotlin.jvm.JvmStatic
+
+actual class Bytes internal actual constructor(value: ByteArray) : AbstractBytes(value) {
+    fun asReadOnlyBuffer(): ByteBuffer =
+        ByteBuffer.wrap(value).asReadOnlyBuffer()
+
+    fun inputStream(): InputStream =
+        value.inputStream()
+
+    actual companion object {
+        @JvmStatic
+        actual fun empty() =
+            AbstractBytes.empty()
+
+        @JvmStatic
+        actual fun from(bytes: ByteArray) =
+            AbstractBytes.from(bytes)
+
+        @JvmStatic
+        actual fun from(message: KtMessage) =
+            AbstractBytes.from(message)
+
+        @JvmStatic
+        fun from(stream: InputStream) =
+            Bytes(stream.readBytes())
+    }
+}
+
+internal actual fun clone(bytes: ByteArray) =
+    bytes.clone()
