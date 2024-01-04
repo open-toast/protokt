e2e0379219a27d22356d9ed4b6ede8b077390bb0
Andrew Parmet <andrew@parmet.com>
2024-01-03 23:52:02 -0500

    lint

--- buildSrc/src/main/kotlin/LocalProtoktBuild.kt
+++ buildSrc/src/main/kotlin/LocalProtoktBuild.kt
@@ -15,10 +15,8 @@
 
 import com.google.protobuf.gradle.GenerateProtoTask
 import org.gradle.api.Project
-import org.gradle.api.tasks.SourceSetContainer
 import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.kotlin.dsl.get
-import org.gradle.kotlin.dsl.the
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.targets.js.dsl.KotlinJsTargetDsl
 import protokt.v1.gradle.CODEGEN_NAME
