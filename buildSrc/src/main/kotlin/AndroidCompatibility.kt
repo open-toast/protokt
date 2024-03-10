6332136efd1ba5e06b78bb84fa0a393670273610
Oleg Golberg <ogolberg@toasttab.com>
2024-03-10 08:58:53 -0400

    expediter

--- buildSrc/src/main/kotlin/AndroidCompatibility.kt
+++ buildSrc/src/main/kotlin/AndroidCompatibility.kt
@@ -16,18 +16,18 @@
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
-import ru.vyarus.gradle.plugin.animalsniffer.AnimalSnifferExtension
-import ru.vyarus.gradle.plugin.animalsniffer.AnimalSnifferPlugin
+
+import com.toasttab.expediter.gradle.ExpediterPlugin
+import com.toasttab.expediter.gradle.config.ExpediterExtension
 
 fun Project.compatibleWithAndroid(api: Int = 19) {
-    apply<AnimalSnifferPlugin>()
+    apply<ExpediterPlugin>()
 
-    configure<AnimalSnifferExtension> {
-        toolVersion = "1.21"
-    }
-
-    dependencies {
-        add("signature", "com.toasttab.android:gummy-bears-api-$api:0.4.0@signature")
+    configure<ExpediterExtension> {
+        platform {
+            android {
+                sdk = api
+            }
+        }
     }
 }
