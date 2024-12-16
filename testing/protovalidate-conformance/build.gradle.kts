68f7521053366fb2dbd451345eaa64c13b264d9c
Andrew Parmet <andrew@parmet.com>
2024-12-16 15:17:53 -0500

    cut down

--- testing/protovalidate-conformance/build.gradle.kts
+++ testing/protovalidate-conformance/build.gradle.kts
@@ -85,7 +85,7 @@ val conformance =
         dependsOn(TASK_INSTALL_NAME, installConformance)
         description = "Runs protovalidate conformance tests."
         environment(
-            "JAVA_OPTS" to "-Xmx64M",
+            "JAVA_OPTS" to "-Xmx45M",
             "GOMEMLIMIT" to "16MiB"
         )
         commandLine(
