3e3f57fcc43b2500e94f56026ce9e52d0fa0d581
Andrew Parmet <andrew@parmet.com>
2024-12-21 00:47:37 -0500

    reintroduce json tests

--- testing/conformance/driver/src/jvmMain/kotlin/protokt/v1/conformance/Platform.kt
+++ testing/conformance/driver/src/jvmMain/kotlin/protokt/v1/conformance/Platform.kt
@@ -23,6 +23,8 @@ import protokt.v1.Message
 import protokt.v1.conformance.ConformanceResponse.Result.ParseError
 import protokt.v1.conformance.ConformanceResponse.Result.RuntimeError
 import protokt.v1.conformance.ConformanceResponse.Result.SerializeError
+import protokt.v1.json.toJson
+import protokt.v1.testing.getContextReflectively
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
@@ -83,15 +85,11 @@ internal actual object Platform {
         ConformanceStepResult.skip()
 
     actual fun serializeJson(message: Message): ConformanceStepResult<String> =
-        ConformanceStepResult.skip()
-    /*
         try {
             Proceed(message.toJson(getContextReflectively()))
         } catch (t: Throwable) {
             Stop(SerializeError(t.stackTraceToString()))
         }
-
-     */
 }
 
 internal fun intToBytes(size: Int) =
