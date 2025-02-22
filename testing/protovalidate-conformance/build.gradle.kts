b6041e4f65bdf758b4af5ab3b1ae6693e42adbdb
Andrew Parmet <andrew@parmet.com>
2025-02-22 15:38:01 -0500

    enable conformance

--- testing/protovalidate-conformance/build.gradle.kts
+++ testing/protovalidate-conformance/build.gradle.kts
@@ -99,7 +99,7 @@ val conformance =
     }
 
 // Super unstable in CI, I guess.
-// tasks.test { dependsOn(conformance) }
+tasks.test { dependsOn(conformance) }
 
 application {
     mainClass.set("protokt.v1.buf.validate.conformance.Main")
