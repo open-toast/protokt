6dbb5df8684fdf55cf57517004bfed866e5bf269
Andrew Parmet <andrew@parmet.com>
2025-02-25 18:06:28 -0500

    more fixes

--- buildSrc/src/main/kotlin/protokt.third-party-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/protokt.third-party-conventions.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Toast, Inc.
+ * Copyright (c) 2022 Toast, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,4 @@
  * limitations under the License.
  */
 
-subprojects {
-    group = "${rootProject.group}.thirdparty"
-}
+group = "com.toasttab.protokt.thirdparty"
