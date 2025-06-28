80e9ee3905d2de2f0162bd032bab0f5dc1772ee3
Andrew Parmet <andrew@parmet.com>
2025-06-27 20:01:01 -0400

    Revert "Implement oneofs as abstract classes instead of sealed classes (#397)"
    This reverts commit 5deee27a3166570ff9767ca2e3e7bfb7b444ec2a.

--- protokt-codegen/src/main/kotlin/protokt/v1/codegen/generate/SerializeAndSizeSupport.kt
+++ protokt-codegen/src/main/kotlin/protokt/v1/codegen/generate/SerializeAndSizeSupport.kt
@@ -106,7 +106,7 @@ private fun oneofInstanceConditionals(f: Oneof, stmt: (StandardField) -> CodeBlo
             buildCodeBlock {
                 addStatement("is·%T·->\n%L", f.qualify(it), stmt(it))
             }
-        } + buildCodeBlock { addStatement("else·->·Unit") }
+        } + buildCodeBlock { addStatement("null·->·Unit") }
 
 internal fun Oneof.qualify(f: StandardField) =
     className.nestedClass(fieldTypeNames.getValue(f.fieldName))
