da8418b73452870f4cf41238f1eb2f133bf999d2
Andrew Parmet <andrew@parmet.com>
2026-02-11 09:23:41 -0500

    upgrade foojay

--- gradle-plugin-integration-test/settings.gradle.kts
+++ gradle-plugin-integration-test/settings.gradle.kts
@@ -23,7 +23,7 @@ buildscript {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 apply(plugin = "net.vivin.gradle-semantic-build-versioning")
