d141b508487630c4173b594ea5bfc56a74730b1c
Andrew Parmet <andrew@parmet.com>
2026-08-31 08:54:56 -0400

    Keep protobuf compatibility injection unchanged

--- shared-src/gradle-plugin/protokt/v1/gradle/ProtoktBuild.kt
+++ shared-src/gradle-plugin/protokt/v1/gradle/ProtoktBuild.kt
@@ -16,6 +16,7 @@
 package protokt.v1.gradle
 
 import com.google.protobuf.gradle.ProtobufExtension
+import com.google.protobuf.gradle.ProtobufPlugin
 import com.google.protobuf.gradle.proto
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
@@ -79,6 +80,15 @@ internal fun configureProtokt(
     project.configureProtobuf(disableJava, config, project.prepareCodegenBinary(binary))
 }
 
+private fun injectKotlinPluginsIntoProtobufGradle() {
+    val prerequisitePluginsField = ProtobufPlugin::class.java.getDeclaredField("PREREQ_PLUGIN_OPTIONS")
+    prerequisitePluginsField.isAccessible = true
+
+    @Suppress("UNCHECKED_CAST")
+    val prerequisitePlugins = prerequisitePluginsField.get(null) as MutableList<String>
+    prerequisitePlugins.add("org.jetbrains.kotlin.multiplatform")
+}
+
 internal class Config(
     val extension: ProtoktExtension,
     val extensions: Configuration,
@@ -99,7 +109,7 @@ private fun Project.configureProtobuf(
     binary: TaskProvider<PrepareCodegenBinary>
 ) {
     pluginManager.withPlugin(KotlinPlugins.MULTIPLATFORM) {
-        ProtobufGradlePluginCompatibility.enableMultiplatform()
+        injectKotlinPluginsIntoProtobufGradle()
         configureForMpp(disableJava, config, binary)
     }
 
